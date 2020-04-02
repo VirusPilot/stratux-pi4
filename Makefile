cedbc077260032bfc1bacd97346c8a8fbeddc46d
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2020-04-02 13:22:53 +0200

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,53 @@
+
+LDFLAGS_VERSION=-X main.stratuxVersion=`git describe --tags --abbrev=0` -X main.stratuxBuild=`git log -n 1 --pretty=%H`
+BUILDINFO=-ldflags "$(LDFLAGS_VERSION)"
+BUILDINFO_STATIC=-ldflags "-extldflags -static $(LDFLAGS_VERSION)"
+
+all:
+	make xdump978 xdump1090 xgen_gdl90 fancontrol www
+
+xgen_gdl90:
+	go get -t -d -v ./main ./godump978 ./uatparse ./sensors
+	export CGO_CFLAGS_ALLOW="-L/root/stratux" && go build $(BUILDINFO) -p 4 main/gen_gdl90.go main/traffic.go main/gps.go main/network.go main/managementinterface.go main/sdr.go main/ping.go main/uibroadcast.go main/monotonic.go main/datalog.go main/equations.go main/sensors.go main/cputemp.go main/lowpower_uat.go main/flarm.go main/flarm-nmea.go main/networksettings.go main/xplane.go
+
+fancontrol:
+	go get -t -d -v ./main
+	go build $(BUILDINFO) -p 4 main/fancontrol.go main/equations.go main/cputemp.go
+
+xdump1090:
+	cd dump1090 && make BLADERF=no
+
+xdump978:
+	cd dump978 && make lib
+	sudo cp -f ./libdump978.so /usr/lib/libdump978.so
+
+www:
+	cd web && make
+
+install:
+	cp -f libdump978.so /usr/lib/libdump978.so
+
+	cp -f gen_gdl90 /usr/bin/gen_gdl90
+	chmod 755 /usr/bin/gen_gdl90
+
+	cp image/10-stratux.rules /etc/udev/rules.d/10-stratux.rules
+	cp image/99-uavionix.rules /etc/udev/rules.d/99-uavionix.rules
+
+	cp -f image/hostapd_manager.sh /usr/sbin/
+	cp -f image/stratux-wifi.sh /usr/sbin/
+	cp -f image/hostapd.conf.template /etc/hostapd/
+	cp -f image/interfaces.template /etc/network/
+	cp -f image/wpa_supplicant.conf.template /etc/wpa_supplicant/
+
+	rm -f /var/run/ogn-rf.fifo
+	mkfifo /var/run/ogn-rf.fifo
+
+	cp -f ogn/rtlsdr-ogn/ogn-rf /usr/bin/
+	chmod a+s /usr/bin/ogn-rf
+	cp -f ogn/rtlsdr-ogn/ogn-decode /usr/bin/
+	cp -f ogn/ddb.json /etc/
+
+clean:
+	rm -f gen_gdl90 libdump978.so fancontrol ahrs_approx
+	cd dump1090 && make clean
+	cd dump978 && make clean
---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#set -x
+
+echo
+read -t 1 -n 10000 discard
+read -p "apt packages already installed? [y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  apt update
+  apt full-upgrade
+  apt install libncurses5-dev -y
+  apt install pkg-config -y
+  apt install libjpeg8-dev -y
+  apt install libconfig9 -y
+  apt install hostapd -y
+  apt install tcpdump -y
+  apt install git -y
+  apt install cmake -y
+  apt install autoconf -y
+  apt install libtool -y
+  apt install i2c-tools -y
+  apt install librtlsdr0 -y
+  apt install librtlsdr-dev -y
+  apt install golang -y
+  wget https://project-downloads.drogon.net/wiringpi-latest.deb
+  wget http://ftp.debian.org/debian/pool/main/f/fftw3/libfftw3-bin_3.3.5-3_armhf.deb
+  wget http://ftp.debian.org/debian/pool/main/f/fftw3/libfftw3-dev_3.3.5-3_armhf.deb
+  wget http://ftp.debian.org/debian/pool/main/f/fftw3/libfftw3-double3_3.3.5-3_armhf.deb
+  wget http://ftp.debian.org/debian/pool/main/f/fftw3/libfftw3-single3_3.3.5-3_armhf.deb
+  dpkg -i *.deb
+  rm *.deb
+  apt-mark hold libfftw3-bin
+  apt-mark hold libfftw3-dev
+  apt-mark hold libfftw3-double3
+  apt-mark hold libfftw3-single3
+fi
+
+cd /root
+rm -r /root/stratux
+git clone https://github.com/b3nn0/stratux.git
+cd /root/stratux
+git clone --branch stratux https://github.com/Determinant/dump1090-fa-stratux.git dump1090
+git submodule update --init --recursive goflying
+
+cd /root/stratux/ogn
+rm -r rtlsdr*
+wget http://download.glidernet.org/arm/rtlsdr-ogn-bin-ARM-latest.tgz
+tar xvzf *.tgz
+rm *.tgz
+
+cp -f /root/stratux-fa/stratux.conf.template /etc/stratux-ogn.conf.template
+cp -f /root/stratux-fa/Makefile /root/stratux/Makefile
+
+cp -f /root/stratux-fa/stratux.service /lib/systemd/system/stratux.service
+chmod 644 /lib/systemd/system/stratux.service
+ln -fs /lib/systemd/system/stratux.service /etc/systemd/system/multi-user.target.wants/stratux.service
+
+cp -f /root/stratux-fa/stratux-pre-start.sh /root/stratux-pre-start.sh
+chmod 744 /root/stratux-pre-start.sh
+
+cp -f /root/stratux-fa/dump1090 /usr/bin/
+chmod 755 /usr/bin/dump1090
+
+cd /root/stratux
+export PATH=/usr/lib/go/bin:${PATH}
+export GOROOT=/usr/lib/go
+export GOPATH=/usr/lib/go_path
+echo export PATH=/usr/lib/go/bin:${PATH} >> ~/.bashrc
+echo export GOROOT=/usr/lib/go >> ~/.bashrc
+echo export GOPATH=/usr/lib/go_path >> ~/.bashrc
+ldconfig
+make && make install
+
+echo
+read -t 1 -n 10000 discard
+read -p "i2c already enabled? [y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  raspi-config
+fi
