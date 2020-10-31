113e1b7603f21ea532c630d71939af1d2fcfcf30
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2020-10-31 19:47:16 +0100

    remove obsolete copying

--- setup-pi4.sh
+++ setup-pi4.sh
@@ -76,10 +76,6 @@ cd /root/stratux
 # replace Makefile (add "-o fancontrol" for go 1.15 compatibility)
 cp -f /root/stratux-pi4/Makefile /root/stratux/Makefile
 
-# copy various files from /root/stratux
-cp __lib__systemd__system__stratux.service /lib/systemd/system/stratux.service
-cp __root__stratux-pre-start.sh /root/stratux-pre-start.sh
-
 # copy various files from /root/stratux/image
 cd /root/stratux/image
 cp -f config.txt /boot/config.txt
