db6916ba5e937f7d9e0279875869539e5acfaa42
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-06-05 18:03:47 +0200

    Update setup-pi4.sh

--- setup-pi4.sh
+++ setup-pi4.sh
@@ -7,7 +7,7 @@ ifconfig wlan0 up
 timedatectl set-timezone Europe/Berlin
 
 # prepare libs
-apt install qemu-utils parted zip unzip zerofree build-essential automake autoconf libncurses-dev pkg-config libjpeg62-turbo-dev libconfig9 hostapd isc-dhcp-server tcpdump git cmake libtool i2c-tools libusb-1.0-0-dev libfftw3-dev python-serial -y
+apt install parted zip unzip zerofree build-essential automake autoconf libncurses-dev pkg-config libjpeg62-turbo-dev libconfig9 hostapd isc-dhcp-server tcpdump git cmake libtool i2c-tools libusb-1.0-0-dev libfftw3-dev python-serial -y
 
 # disable swapfile
 systemctl disable dphys-swapfile
