cfd3cbb5fdf73ac8301e2a0137ba3ce68da14fcc
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-06-05 19:42:54 +0200

    Update setup-pi4.sh
    go 1.16.5

--- setup-pi4.sh
+++ setup-pi4.sh
@@ -36,7 +36,7 @@ ldconfig
 
 # install latest golang
 cd /root
-wget https://golang.org/dl/go1.16.4.linux-arm64.tar.gz
+wget https://golang.org/dl/go1.16.5.linux-arm64.tar.gz
 rm -rf /root/go
 rm -rf /root/go_path
 tar xzf *.gz
