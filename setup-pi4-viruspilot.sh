3cbd614be059c62c40c41a4e76d578324705efa8
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2023-12-08 17:20:34 +0100

    remove old librtlsdr libs

--- setup-pi4-viruspilot.sh
+++ setup-pi4-viruspilot.sh
@@ -19,9 +19,17 @@ rm -rf /root/go_path
 tar xzf *.gz
 rm *.gz
 
-# install librtlsdr
+# compile and install librtlsdr from https://github.com/osmocom/rtl-sdr fork
 cd /root
+apt purge ^librtlsdr
 rm -rf /root/rtl-sdr
+rm -rf /usr/lib/librtlsdr*
+rm -rf /usr/include/rtl-sdr*
+rm -rf /usr/local/lib/librtlsdr*
+rm -rf /usr/local/lib/aarch64-linux-gnu/librtlsdr*
+rm -rf /usr/local/include/rtl-sdr*
+rm -rf /usr/local/include/rtl_*
+rm -rf /usr/local/bin/rtl_*
 git clone https://github.com/VirusPilot/rtl-sdr.git
 cd rtl-sdr
 mkdir build
